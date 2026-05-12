1e3bd6f3322c5ff64a02d775009bf4ba95b37560
hjuster <filip.arneric@gmail.com>
2026-05-12 17:49:51 +0200

    custom domain implemented

--- app/prismic/client.js
+++ app/prismic/client.js
@@ -3,7 +3,9 @@ import { repositoryName } from "~/slicemachine.config.json"
 
 export const cachelyPrismicClient = createCachelyPrismicClient({
   repositoryName,
-  tenant: "prismic"
+  tenant: "prismic",
+  customDomain: true
+  // cachelyEndpoint: "https://prismic-cachely.synchronized.studio/~api/api/v2"
 })
 
 export default cachelyPrismicClient
