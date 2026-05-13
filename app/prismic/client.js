b86444b63d1cf41c0e1edda31fd5bd6323680777
hjuster <filip.arneric@gmail.com>
2026-05-13 04:49:11 +0200

    new sdk

--- app/prismic/client.js
+++ app/prismic/client.js
@@ -1,10 +1,18 @@
-import { createCachelyPrismicClient } from "@cachely-io/sdk/prismic"
+import {
+  createCachelyPrismicClient,
+  createCachelyExperimentTracker
+} from "@cachely-io/sdk/prismic"
 import { repositoryName } from "~/slicemachine.config.json"
+
+export const cachelyExperiments = createCachelyExperimentTracker({
+  tenant: "prismic"
+})
 
 export const cachelyPrismicClient = createCachelyPrismicClient({
   repositoryName,
   tenant: "prismic",
-  customDomain: true
+  customDomain: true,
+  experiments: cachelyExperiments
   // cachelyEndpoint: "https://prismic-cachely.synchronized.studio/~api/api/v2"
 })
 
